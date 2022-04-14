22f2aed818fcd4a82df6c6df000d602ff52a1a4e
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-04-14 14:27:08 +0300

    feat: add tests

---
+++ __tests__/index.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@jest/globals';
+import buildSearchEngine from '../src/index.js';
+
+const doc1 = { id: 'doc1', text: "I can't SHooT straight unless I've had a pint!" };
+const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+const doc3 = { id: 'doc3', text: "I'm your shooter." };
+const docs = [doc1, doc2, doc3];
+
+const expectedResults = ['doc1', 'doc2'];
+
+test('clear search', () => {
+  const searchEngineWithDocs = buildSearchEngine(docs);
+  const searchEngineWithoutDocs = buildSearchEngine([]);
+
+  expect(searchEngineWithDocs.search('shoot')).toEqual(expectedResults);
+  expect(searchEngineWithDocs.search('mismatch')).toEqual([]);
+  expect(searchEngineWithDocs.search('')).toEqual([]);
+  expect(searchEngineWithoutDocs.search('shoot')).toEqual([]);
+  expect(searchEngineWithoutDocs.search('')).toEqual([]);
+});
