608ed9799e2be70cca03ee34bf58ab2acf8e441e
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-04-14 17:09:50 +0300

    feat: exlude an impact of punctuation marks in a query and text

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const buildSearchEngine = (docs = []) => {
-  const search = (query) => docs
-    .filter(({ text }) => {
-      const matches = text
-        .split(' ')
-        .filter((word) => word.toLowerCase() === query);
-      return matches.length > 0;
-    })
-    .map(({ id }) => id);
+  const search = (token) => {
+    const term = token.match(/\w+/g);
+    if (!term) {
+      return [];
+    }
+    return docs
+      .filter(({ text }) => {
+        const matches = text.match(/\w+/g);
+        const normalizedMatches = matches.filter((t) => t.toLowerCase() === term[0].toLowerCase());
+        return normalizedMatches.length > 0;
+      })
+      .map(({ id }) => id);
+  };
 
   return {
     search,
