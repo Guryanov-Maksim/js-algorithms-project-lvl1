fecc7d3bb23bfe91619084f484381f1c85d0c57a
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-04-15 11:34:05 +0300

    feat: add a relevant search

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ const buildSearchEngine = (docs = []) => {
     if (!term) {
       return [];
     }
-    return docs
-      .filter(({ text }) => {
-        const matches = text.match(/\w+/g);
-        const normalizedMatches = matches.filter((t) => t.toLowerCase() === term[0].toLowerCase());
-        return normalizedMatches.length > 0;
-      })
+    const regex = new RegExp(`\\b${term[0]}(?!(\\w+))\\b`, 'gi');
+    const rankedDoc = docs.map(({ id, text }) => {
+      // const matches = text.match(/\w+/g);
+      const matches = text.match(regex);
+      // console.log(matches);
+      // const normalizedMatches = matches.filter((t) => t.toLowerCase() === term[0].toLowerCase());
+      // return normalizedMatches.length > 0;
+      if (matches) {
+        return { id, text, rank: matches.length };
+      }
+      return { id, text, rank: 0 };
+    });
+    const matchedDocs = rankedDoc.filter((doc) => doc.rank > 0);
+    const result = matchedDocs
+      .sort((a, b) => b.rank - a.rank)
       .map(({ id }) => id);
+    return result;
   };
 
   return {
