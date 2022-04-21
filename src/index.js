f2e27d5d9d5b65ebd4fb57aa986857e9ce14eaff
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-04-21 11:48:54 +0300

    feat: add inverted index for saved docs

--- src/index.js
+++ src/index.js
@@ -1,61 +1,84 @@
-const rankDocuments = (docs, terms) => {
-  const regexes = terms.map((term) => new RegExp(`\\b${term}(?!(\\w+))\\b`, 'gi'));
-  const result = docs.map(({ id, text }) => {
-    const rankedDoc = {
-      id,
-      text,
-      matchedWordsCount: 0,
-      inclusionCount: 0,
-    };
-    for (let i = 0; i < regexes.length; i += 1) {
-      const regex = regexes[i];
-      const matches = text.match(regex);
-      if (matches) {
-        rankedDoc.matchedWordsCount += 1;
-        rankedDoc.inclusionCount += matches.length;
+import _ from 'lodash';
+
+const buildInvertedIndex = (docs) => {
+  // const serchedDocs = new RegExp(`\\b[${term}\`](?!(\\w+))\\b`, 'gi');
+  const result = docs.reduce((acc, { id, text }) => {
+    const words = [...text.match(/[\w+']+/gi)];
+    let newAcc = { ...acc };
+    for (let i = 0; i < words.length; i += 1) {
+      const word = words[i].toLowerCase();
+      if (newAcc[word]) {
+        const { ids, inclusionCounts } = newAcc[word];
+        ids.add(id);
+        const prevCount = _.get(inclusionCounts, id, 0);
+        newAcc = {
+          ...newAcc,
+          [word]: {
+            ids,
+            inclusionCounts: {
+              ...inclusionCounts,
+              [id]: prevCount + 1,
+            },
+          },
+        };
+      } else {
+        const ids = new Set();
+        ids.add(id);
+        newAcc = {
+          ...newAcc,
+          [word]: {
+            ids,
+            inclusionCounts: {
+              [id]: 1,
+            },
+          },
+        };
       }
     }
 
-    return rankedDoc;
-    // const matches = text.match(/\w+/g);
-    // const matches = text.match(regex);
-    // console.log(matches);
-    // const normalizedMatches = matches.filter((t) => t.toLowerCase() === term[0].toLowerCase());
-    // return normalizedMatches.length > 0;
-    // if (matches) {
-    //   return { id, text, rank: matches.length };
-    // }
-    // return { id, text, rank: 0 };
-  });
+    return newAcc;
+  }, {});
 
   return result;
 };
 
+const rankDocuments = (index, terms) => {
+  const iter = (wordsToMatch, result) => {
+    if (wordsToMatch.length === 0) {
+      return _.union(...result);
+    }
+    const allMatchedDocIds = wordsToMatch.map((word) => {
+      const ids = _.get(index[word], 'ids', []);
+      return Array.from(ids);
+    });
+    const docIdsWithAllWords = _.intersection(...allMatchedDocIds);
+
+    const docIdsWithWordCount = docIdsWithAllWords.map((id) => {
+      const totalWordsCount = wordsToMatch.reduce((acc, word) => {
+        const newAcc = acc + index[word].inclusionCounts[id];
+        return newAcc;
+      }, 0);
+      return ({ id, totalWordsCount });
+    }, []);
+    const sortedIds = docIdsWithWordCount.sort((a, b) => b.totalWordsCount - a.totalWordsCount);
+    const ids = sortedIds.map((item) => item.id);
+
+    return iter(wordsToMatch.slice(0, wordsToMatch.length - 1), [...result, ids]);
+  };
+
+  return iter([...terms], []);
+};
+
 const buildSearchEngine = (docs = []) => {
+  const index = buildInvertedIndex(docs);
+
   const search = (token) => {
     const terms = token.match(/\w+/g);
     if (!terms) {
       return [];
     }
-    const rankedDocs = rankDocuments(docs, terms);
-    const matchedDocs = rankedDocs.filter((doc) => doc.matchedWordsCount > 0);
-    // const docsSortedByWordsCount = matchedDocs
-    //   .sort((a, b) => b.matchedWordsCount - a.matchedWordsCount);
-    const groupedByWordCount = matchedDocs.reduce((acc, item) => {
-      const { matchedWordsCount } = item;
-      if (acc[matchedWordsCount]) {
-        return { ...acc, [matchedWordsCount]: [...acc[matchedWordsCount], item] };
-      }
-      return { ...acc, [matchedWordsCount]: [item] };
-    }, {});
-    const sortedWordCounts = Object.keys(groupedByWordCount).sort((a, b) => b - a);
-
-    const result = sortedWordCounts
-      .reduce((acc, count) => (
-        [...acc, ...groupedByWordCount[count].sort((a, b) => b.inclusionCount - a.inclusionCount)]
-      ), [])
-      .map(({ id }) => id);
-    return result;
+    const rankedDocsIds = rankDocuments(index, terms);
+    return rankedDocsIds;
   };
 
   return {
