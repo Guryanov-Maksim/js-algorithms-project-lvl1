e9abdc39f099f5f8539789c7f8b9157d3e9fd49d
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-04-22 08:12:26 +0300

    feat: update tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -5,11 +5,12 @@ const doc1 = { id: 'doc1', text: "I can't SHooT straight unless I've had a pint!
 const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at him." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const doc4 = { id: 'doc4', text: "Don't shoot that thing at me." };
-const docs = [doc1, doc2, doc3, doc4];
+const doc5 = { id: 'doc5', text: 'Please shoot that thing at a small bottle.' };
+const docs = [doc1, doc2, doc3, doc4, doc5];
 
-const expectedResults = ['doc2', 'doc1', 'doc4'];
+const expectedResults = ['doc2', 'doc1', 'doc4', 'doc5'];
 const expectedResults2 = ['doc1'];
-const expectedResults3 = ['doc4', 'doc2', 'doc1'];
+const expectedResults3 = ['doc4', 'doc5', 'doc2', 'doc1'];
 
 test('clear search', () => {
   const searchEngineWithDocs = buildSearchEngine(docs);
